ab7c56733237dad5e70cc41c02729f712b993319
Ivan Cherviakovskiy <ivancherviakovskiy@gmail.com>
2024-09-04 16:39:35 +0300

    add validation pipe to review contoller.

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,7 +1,16 @@
+import { IsNumber, IsString, Max, Min } from 'class-validator'
+
 export class CreateReviewDto {
+    @IsString()
     name: string;
+    @IsString()
     title: string;
+    @IsString()
     description: string;
+    @Min(1, { message: 'Значение рейтинга не может быть ниже 1' })
+    @Max(5)
+    @IsNumber()
     rating: number;
+    @IsString()
     productId: string;
 }
--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,15 +1,17 @@
-import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, ValidationPipe } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
+
 
 @Controller('review')
 export class ReviewController {
 
     constructor(private readonly reviewService: ReviewService) { }
 
+
     @Post('create')
-    async create(@Body() dto: Omit<CreateReviewDto, '_id'>) {
+    async create(@Body(new ValidationPipe()) dto: Omit<CreateReviewDto, '_id'>) {
         return this.reviewService.create(dto);
     }
 
